dbc2714579cafd9e94c5fb68aae54bc01438ac11
atulverma4796 <atulverma4796@gmail.com>
2022-08-26 11:34:32 +0530

    add skeleton

--- src/showContact.js
+++ src/showContact.js
@@ -1,49 +1,52 @@
 import React, { useEffect, useState } from "react";
-import './index.css';
+import "./index.css";
 import GoToAdd from "./goToAdd";
-import { confirmAlert } from 'react-confirm-alert'; // Import
-import 'react-confirm-alert/src/react-confirm-alert.css';
+import { confirmAlert } from "react-confirm-alert"; // Import
+import "react-confirm-alert/src/react-confirm-alert.css";
 import { Link } from "react-router-dom";
-
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const ShowContact = () => {
-  const [contactData, setContactData] = useState([])
+  const [contactData, setContactData] = useState([]);
   useEffect(() => {
-    let data = JSON.parse(localStorage.getItem('contact'));
+    let data = JSON.parse(localStorage.getItem("contact"));
     if (data !== null && data.length !== 0) {
-      setContactData(data)
+      setTimeout(() => {
+        setContactData(data);
+      }, 2000);
     }
-  }, [])
+  }, []);
 
   const deleteContact = () => {
     confirmAlert({
-      title: 'Confirm to submit',
-      message: 'Are you sure to do this.',
+      title: "Confirm to submit",
+      message: "Are you sure to do this.",
       buttons: [
         {
-          label: 'Yes',
+          label: "Yes",
           onClick: (index) => {
-            let data = JSON.parse(localStorage.getItem('contact'));
+            let data = JSON.parse(localStorage.getItem("contact"));
             if (data !== null && data.length !== 0) {
-              data.splice(index, 1)
+              data.splice(index, 1);
               // console.log(data)
               // setContactData(data)
-              localStorage.setItem('contact', JSON.stringify(data));
-              window.location = "/"
+              localStorage.setItem("contact", JSON.stringify(data));
+              window.location = "/";
             }
-          }
+          },
         },
         {
-          label: 'No',
+          label: "No",
           onClick: () => {
             // window.location="/"
-          }
-        }
-      ]
+          },
+        },
+      ],
     });
-  }
-  return <>
-    {contactData.length !== 0 ?
+  };
+  return (
+    <>
       <div className="contact_table">
         <div className="header">
           <h3 className="table_caption">Contact Detail Table</h3>
@@ -63,23 +66,40 @@ const ShowContact = () => {
             </tr>
           </thead>
           <tbody>
-            {contactData.map((value, index) => <tr key={index}>
-              <td>{index + 1}</td>
-              <td>{value.name}</td>
-              <td>{value.phone}</td>
-              <td>{value.type}</td>
-              <td>{value.isWhatsapp ? 'Yes' : 'No'}</td>
-              <td>
-                <Link to={`/addContact/${value.id}`}>
-                  <button className="edit_button">Edit</button>
-                </Link>
-                <button className="delete_button" onClick={() => deleteContact(index)}>Delete</button>
-              </td>
-            </tr>)}
+            {contactData.length !== 0 ? (
+              <>
+                {contactData.map((value, index) => (
+                  <tr key={index}>
+                    <td>{index + 1}</td>
+                    <td>{value.name}</td>
+                    <td>{value.phone}</td>
+                    <td>{value.type}</td>
+                    <td>{value.isWhatsapp ? "Yes" : "No"}</td>
+                    <td>
+                      <Link to={`/addContact/${value.id}`}>
+                        <button className="edit_button">Edit</button>
+                      </Link>
+                      <button
+                        className="delete_button"
+                        onClick={() => deleteContact(index)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </>
+            ) : (
+              <>
+                <div className="skeleton">
+                  <Skeleton count={5} width="1000px" />
+                </div>
+              </>
+            )}
           </tbody>
         </table>
       </div>
-      : <div className=""><GoToAdd /></div>}
-  </>
-}
+    </>
+  );
+};
 export default ShowContact;
